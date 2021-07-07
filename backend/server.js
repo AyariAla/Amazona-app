4cf38ee883193da68c31f89d3d0096fa9b089c70
Ala <alaaeddine.ayari1@esprit.tn>
2021-07-07 13:30:48 +0100

    first commit

---
+++ backend/server.js
@@ -0,0 +1,53 @@
+import express from "express";
+import mongoose from "mongoose";
+import userRouter from "./routers/userRouter.js";
+import productRouter from "./routers/productRouter.js";
+import dotenv from "dotenv";
+import orderRouter from "./routers/orderRouter.js";
+
+//JWT secret from the .env
+dotenv.config();
+
+const app = express();
+app.use(express.json()); //parsing json data in the body of request
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/amazona", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+
+/* Static data
+app.get('/api/products', (req, res) => {
+    res.send(data.products);
+});
+*/
+
+/* Static product:id fetch from data file
+app.get('/api/products/:id', (req, res) => {
+    const product = data.products.find((x) => x._id === req.params.id);
+    if (product) {
+        res.send(product);
+    } else {
+        res.status(404).send({ message: ' Product not found' });
+    }
+
+});
+*/
+app.use("/api/users", userRouter);
+app.use("/api/products", productRouter);
+app.use("/api/order", orderRouter);
+app.get("/api/config/paypal", (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID || "sb");
+});
+app.get("/", (req, res) => {
+  res.send(" Server is ready");
+});
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Serve at http://localhost:${port}`);
+});
---
+++ backend/utils.js
@@ -0,0 +1,46 @@
+import jwt from "jsonwebtoken";
+
+//Middlewares
+export const generateToken = (user) => {
+  return jwt.sign(
+    {
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    },
+    process.env.JWT_SECRET || "somethingsecret",
+    {
+      expiresIn: "30d",
+    }
+  );
+};
+
+export const isAuth = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (authorization) {
+    const token = authorization.slice(7, authorization.length); // BEARER XXX getting the token part from the authorization from pos 7
+    jwt.verify(
+      token,
+      process.env.JWT_SECRET || "somethingsecret",
+      (err, decode) => {
+        if (err) {
+          res.status(401).send({ message: "Invalid token " });
+        } else {
+          req.user = decode;
+          next();
+        }
+      }
+    );
+  } else {
+    res.status(401).send({ message: "No token " });
+  }
+};
+
+export const isAdmin = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    res.status(401).send({ message: "Invalid admin token" });
+  }
+};
